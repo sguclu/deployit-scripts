9f3100e287d6880a593f8b2d47341a3f13780b24
almbox <almbox@DEPLOYNODE.(none)>
2013-11-17 22:59:18 +0100

    update for duplicates env/non-env elements

--- create-application/create-application.py
+++ create-application/create-application.py
@@ -10,6 +10,8 @@ nonEnvRoles = [ 'IMPORTER' ]
 envRoles = [ 'SYSADMIN', 'OPERATOR', 'APPADMIN' ]
 roles = nonEnvRoles + envRoles
 # authorization matrix rules
+globalprivileges = {}
+appprivileges = {}
 globalprivileges['SYSADMIN'] = [ "discovery", "login", "report#view" ]
 globalprivileges['APPADMIN'] = [ "login", "report#view" ]
 globalprivileges['OPERATOR'] = [ "login", "report#view", "task#assign", "task#preview_step" ]
@@ -19,14 +21,32 @@ appprivileges['OPERATOR'] = { 'Applications' : [ "read" ], 'Infrastructure': [],
 appprivileges['APPADMIN'] = { 'Applications' : [ "controltask#execute", "import#initial", "import#remove", "import#upgrade", "read", "repo#edit" ], 'Environments' : [ "controltask#execute", "read", "repo#edit" ], 'Configuration' : [ "controltask#execute", "read", "repo#edit" ] }
 appprivileges['IMPORTER'] = { 'Applications' : [ "controltask#execute", "import#initial", "import#upgrade" ], 'Infrastructure' : [], 'Environments' : [], 'Configuration' : [] }
 
-# generic create function
+# generic CI create function
 def create(id, type):
   if not repository.exists(id):
     repository.create(factory.configurationItem(id, type, {}))
 
+# role creation and global rights assignment function
+def rolecreate(roletype,roleid):
+    print " -- create role " + roleid
+    security.assignRole(roleid, [])
+    # global privileges attribution for each role instance
+    print " ---- global grants given : ",
+    for globalprivilege in globalprivileges[roletype]:
+      print globalprivilege + " ",
+      security.grant(globalprivilege,roleid)
+    print ""
+
+# gives "read" write to a given role on a given path
+def grantreaddir(roleid,path):
+  print roleid,
+  security.grant("read",roleid, [ path ])
+
 # MAIN CODE
 
 # checks the mandatory parameters
+print "######### APPLICATION CREATION SCRIPT #########"
+print ""
 try:
   department = sys.argv[1].upper()
   application = sys.argv[2].upper()
@@ -43,63 +63,69 @@ for i in range(len(departmentpaths)):
   if (i>0): departmentpaths[i]=departmentpaths[i-1]+'/'+departmentpaths[i]
 
 # creates all the roles required for this application
-for env in environments:
-  for role in roles:
-    # roles created : for each application the role is instanciated on each environment except nonEnvRoles ones
-    if (role in nonEnvRoles):  app_env_role = application + "_" + role
-    else:  app_env_role = application + "_" + env + "_" + role
-    print " -- create role " + app_env_role
-    security.assignRole(app_env_role, [])
-    # global privileges attribution for each role instance
-    for globalprivilege in globalprivileges[role]:
-      print " ---- grant " + globalprivilege + " to " + app_env_role
-      security.grant(globalprivilege,app_env_role)
-  print ""
+print " 1 - ROLES CREATION AND GLOBAL RIGHTS SETUP"
+for role in roles:
+  # roles created : for each application the role is instanciated on each environment except nonEnvRoles ones
+    if (role in nonEnvRoles):  rolecreate(role, application + "_" + role)
+    else:
+      for env in environments: rolecreate(role,application + "_" + env + "_" + role)
+print ""
 
 # creates the Applications, Infrastructure, Environments and Configuration intermediate directories
+print " 2 - INTERMEDIATE DIRECTORIES CREATION AND RIGHTS SETUP"
 for item in items:
   for path in departmentpaths:
     temppath = item + "/" + path
     print " -- create directory : " + temppath
     create(temppath, "core.Directory")
+    print " ---- read rights granted to : ",
     # applies read privilege for all roles
-    for env in environments:
-      for role in roles:
-        if (role in nonEnvRoles):  app_env_role = application + "_" + role
-        else:  app_env_role = application + "_" + env + "_" + role
-        print " ---- giving 'read' privilege to : " + app_env_role
-        security.grant("read",app_env_role, [ temppath ])
-  print ""
+    for role in roles:
+      if (role in nonEnvRoles):
+        grantreaddir(application + "_" + role, temppath)
+      else:  
+        for env in environments:  grantreaddir(application + "_" + env + "_" + role, temppath)
+    print ""
+print ""
 
-#Create "Applications" rights
-directory =  "Applications/" + department + "/" + application
-create( directory, "core.Directory")
-for role in roles:
-  for env in environments:
-    if ( role in nonEnvRoles): app_env_role = application + "_" + role
-    else: app_env_role = application + "_" + env + "_" + role
-    roleprivileges=appprivileges[role].get("Applications")
-    if (roleprivileges is not None):
-      for privilege in roleprivileges:
-        print "---- giving'" + privilege + "' to '" + app_env_role + "' role on directory : " + directory
-        security.grant(privilege, app_env_role, [directory])
-  print ""
+# Create "non environment entities" rights
+print " 3 - NON-ENVIRONMENT ENTITIES RIGHTS SETUP"
+for item in nonEnvItems:
+  directory =  item + "/" + department + "/" + application
+  print " -- create directory : " + directory
+  create( directory, "core.Directory")
+  for role in roles:
+    for env in environments:
+      if ( role in nonEnvRoles): app_env_role = application + "_" + role
+      else: app_env_role = application + "_" + env + "_" + role
+      roleprivileges=appprivileges[role].get(item)
+      if (roleprivileges is not None):
+        print " ---- given privileges to " + app_env_role + " role :  ",
+        for privilege in roleprivileges:
+          security.grant(privilege, app_env_role, [directory])
+          print privilege,
+        print ""
+print ""
 
 # binds application roles with "final" directories
-for item in items:
+print " 4 - ENVIRONMENT ENTITIES RIGHTS SETUP"
+for item in envItems:
   directoryList = []
-  if (item in envItems):
-    for environment in environments:
-      directoryList.append(item + "/" + department + "/" + application + "/" + environment)
-    for directory in directoryList:
-      print "-- creating directory : " + directory
-      create(directory, "core.Directory")
-      for role in roles:
-        if ( role in nonEnvRoles): app_env_role = application + "_" + role
-        else: app_env_role = application + "_" + directory.split("/")[-1] + "_" + role
-        roleprivileges=appprivileges[role].get(item)
-        if (roleprivileges is not None):
-          for privilege in roleprivileges:
-            print "---- giving'" + privilege + "' to '" + app_env_role + "' role on directory : " + directory
-            security.grant(privilege, app_env_role, [directory])
+#  if (item in envItems):
+  for environment in environments:
+    directoryList.append(item + "/" + department + "/" + application + "/" + environment)
+  for directory in directoryList:
+    print " -- create directory : " + directory
+    create(directory, "core.Directory")
+    for role in roles:
+      if ( role in nonEnvRoles): app_env_role = application + "_" + role
+      else: app_env_role = application + "_" + directory.split("/")[-1] + "_" + role
+      roleprivileges=appprivileges[role].get(item)
+      if (roleprivileges is not None):
+        print " ---- given privileges to '" + app_env_role + " role : ",
+        for privilege in roleprivileges:
+          security.grant(privilege, app_env_role, [directory])
+          print privilege,
+        print ""
+print ""
 
